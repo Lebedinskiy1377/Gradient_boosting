9352e7fb8da623858105e6a6085456b398ca39a4
Lebedinskiy1377 <126768270+Lebedinskiy1377@users.noreply.github.com>
2025-07-02 16:25:03 +0300

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def mse(y_true: np.ndarray, y_pred: np.ndarray) -> Tuple[float, np.ndarray]:
     """Mean squared error loss function and gradient."""
-    # YOUR CODE HERE
+    
     loss = np.mean((y_true - y_pred) ** 2)
     grad = y_pred - y_true
     return loss, grad
@@ -13,7 +13,6 @@ def mse(y_true: np.ndarray, y_pred: np.ndarray) -> Tuple[float, np.ndarray]:
 def mae(y_true: np.ndarray, y_pred: np.ndarray) -> Tuple[float, np.ndarray]:
     """Mean absolute error loss function and gradient."""
 
-    # YOUR CODE HERE
     def diff(x: float):
         if x > 0:
             return 1
